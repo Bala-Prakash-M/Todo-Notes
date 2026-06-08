8f5311634dab6c4dfc2265bff72bd4dfb4c95d0b
Bala-Prakash-M <bala.esec2024@gmail.com>
2026-06-08 11:44:52 +0530

    refactor: update notes routes to use NotesController, NotesService, and NotesRepository

--- Backend/src/modules/notes/notes.routes.ts
+++ Backend/src/modules/notes/notes.routes.ts
@@ -3,9 +3,9 @@ import express from "express";
 import { AuthMiddleware } from "../../middlewares/auth.middleware.js";
 import { JwtUtils } from "../../shared/utils/jwt.js";
 
-import { NotebookController } from "../notebook/notebook.controller.js";
-import { NotebookService } from "../notebook/notebook.service.js";
-import { NotebookRepository } from "../../shared/repositories/notebook.repository.js";
+import { NotesController } from "./notes.controller.js";
+import { NotesService } from "./notes.service.js";
+import { NotesRepository } from "../../shared/repositories/notes.repository.js";
 
 const router = express.Router();
 
@@ -13,14 +13,14 @@ const authMiddleware = new AuthMiddleware(
   new JwtUtils()
 );
 
-const notebookController = new NotebookController(
-  new NotebookService(
-    new NotebookRepository()
+const notesController = new NotesController(
+  new NotesService(
+    new NotesRepository()
   ),
 );
 
 router.use(authMiddleware.authenticate);
 
-router.get('/', notebookController.getAll);
+router.get('/', notesController.getAll);
 
 export default router;
